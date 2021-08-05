8127a0b9115c16f29b1f45aa1d6299e55270ec75
my name <myemailid@domain.com>
2021-08-05 16:47:12 +0200

    kommetering af kode til firebase øvelse

--- App.js
+++ App.js
@@ -6,6 +6,75 @@ import LoginForm from "./components/LoginForm";
 import ProfileScreen from "./components/ProfileScreen";
 import { Card } from 'react-native-paper';
 
+// Herunder foregår konfiguration til firebase. .
+const firebaseConfig = {
+  apiKey: "AIzaSyAts5zYxJljKY67teZC8XpxV-JNXNy_pzs",
+  authDomain: "oevelse5updated.firebaseapp.com",
+  projectId: "oevelse5updated",
+  storageBucket: "oevelse5updated.appspot.com",
+  messagingSenderId: "215574406693",
+  appId: "1:215574406693:web:967941b372d7eee9f041cc",
+  measurementId: "G-1QX8B2E9H7"
+};
+
+//onAuthstatechanged er en prædefineret metode, forsynet af firebase, som konstant observerer brugerens status (logget ind vs logget ud)
+//Pba. brugerens status foretages et callback i form af setUSer metoden, som håndterer user-state variablens status.
+function onAuthStateChange(callback) {
+  return firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      callback({loggedIn: true, user: user});
+    } else {
+      callback({loggedIn: false});
+    }
+  });
+}
+
+
+
+
+function App() {
+//Her oprettes bruger state variblen
+  const [user, setUser] = useState({ loggedIn: false });
+
+
+//Her oprettes gæstekomponentsindhold, der udgøres af sign-up og login siderne
+  const GuestPage =
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Opret eller Login med din firebase Email
+        </Text>
+        <Card>
+          <SignUpForm />
+        </Card>
+        <Card>
+          <LoginForm />
+        </Card>
+      </View>;
+
+  //Koden sikrer at kun én Firebase initieres under brug af appen.
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  //useEffect sørger for at vi kan ekskverer sideeffekter i funktionelle konmponenter
+  //Sideeffekter kan være initial fetching af data fra et endpoint ved instantiering af en komponent.
+
+  // læs mere om useEffect her: https://reactjs.org/docs/hooks-effect.html
+
+  //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //Slutteligt envendes en tenary operator, som ud fra brugerens status viser enten ProfileScreen eller GuestPage
+  return user.loggedIn ? <ProfileScreen /> : GuestPage ;
+
+}
+
+//Lokal styling til brug i App.js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,58 +91,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAts5zYxJljKY67teZC8XpxV-JNXNy_pzs",
-  authDomain: "oevelse5updated.firebaseapp.com",
-  projectId: "oevelse5updated",
-  storageBucket: "oevelse5updated.appspot.com",
-  messagingSenderId: "215574406693",
-  appId: "1:215574406693:web:967941b372d7eee9f041cc",
-  measurementId: "G-1QX8B2E9H7"
-};
 
-function onAuthStateChange(callback) {
-  return firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      callback({loggedIn: true, user: user});
-    } else {
-      callback({loggedIn: false});
-    }
-  });
-}
-
-const GuestPage =
-    <View style={styles.container}>
-  <Text style={styles.paragraph}>
-    Opret eller Login med din firebase Email
-  </Text>
-  <Card>
-    <SignUpForm />
-  </Card>
-  <Card>
-    <LoginForm />
-  </Card>
-</View>;
-
-
-function App() {
-
-  const [user, setUser] = useState({ loggedIn: false });
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChange(setUser);
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
-  return user.loggedIn ? <ProfileScreen /> : GuestPage ;
-
-}
-
-
-
+// eksport af App.js
 export default App
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -9,26 +9,20 @@ import {
 } from 'react-native';
 import firebase from 'firebase';
 
-const styles = StyleSheet.create({
-    error: {
-        color: 'red',
-    },
-    inputField: {
-        borderWidth: 1,
-        margin: 10,
-        padding: 10,
-    },
-    header: {
-        fontSize: 40,
-    },
-});
-
 function LoginForm() {
+
+    //Instantiering af statevariabler, der skal benyttes i LoginForm
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [isCompleted, setCompleted] = useState(false)
     const [errorMessage, setErrorMessage] = useState(null)
 
+    /*
+    * Metoden herunder håndterer login af en eksisterende bruger ved at anvende den prædefinerede metode, som stilles til rådighed af firebase
+    * signInWithEmailAndPassword tager en mail og et password med som argumenter og foretager et asynkront kald, der eksekverer login i firebase
+    * Opstår der fejl under forsøget på login, vil der i catch blive fremsat en fejlbesked, som, ved brug af
+    * setErrorMessage, angiver værdien for state-variablen, errormessage
+    */
     const handleSubmit = async () => {
         try {
             await firebase.auth().signInWithEmailAndPassword(email, password).then((data)=>{
@@ -39,12 +33,14 @@ function LoginForm() {
         }
     }
 
+    //Her defineres loginknappen, som aktiverer handleSubmit igennem onPress
     const renderButton = () => {
         return <Button onPress={() => handleSubmit()} title="Login" />;
     };
-    if (isCompleted) {
-        return <Text>You are now logged in</Text>;
-    }
+
+//I return oprettes en tekstkomponent, der angiver at dette er loginfrom
+//Dernæst er der to inputfelter, som løbeende sætter værdien af state-variablerne, mail og password.
+// Afslutningsvis, angives det at, hvis errorMessage får fastsat en værdi, skal denne udskrives i en tekstkomponent.
     return (
         <View>
             <Text style={styles.header}>Login up</Text>
@@ -69,4 +65,20 @@ function LoginForm() {
     );
 }
 
+//Lokal styling til brug i LoginFrom
+const styles = StyleSheet.create({
+    error: {
+        color: 'red',
+    },
+    inputField: {
+        borderWidth: 1,
+        margin: 10,
+        padding: 10,
+    },
+    header: {
+        fontSize: 40,
+    },
+});
+
+//Eksport af Loginform, således denne kan importeres og benyttes i andre komponenter
 export default LoginForm
